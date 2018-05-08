7321b288e761e374899d4be5f39f4e1287e58110
npwebste <30417327+npwebste@users.noreply.github.com>
2018-05-08 13:16:39 -0700

    SCIP Update

--- dev/05_05_2018/UPS_SCIP.py
+++ dev/05_05_2018/UPS_SCIP.py
@@ -3,9 +3,11 @@ from pyscipopt import Model, quicksum
 #Initialize model
 model = Model("UPS_Controller")
 
-Solar_Forecast = [500,1000]
-Water_Forecast = [1,1.5]
-P_Grid = 2500
+time = [0, 1]
+
+Solar_Forecast = [100,1000]
+Water_Forecast = [.004,.008]
+P_Grid = 2000
 Theta_Max = 314
 eta = 1
 
@@ -20,98 +22,72 @@ P_Osmotic = 377490
 s_opt = 1
 hmax = 1.2
 
-weight1 = .01
-weight2 = .01
-weight3 = .001
-weight4 = .01
-
 # Create variables
-P_Solar_Ref, P_In_G, P_In_S, P_RO, st, Fp, Yopt, P_Sys, Vf, hdot, h, Theta_VFD, Vr = {}
+P_Solar_Ref, P_In_G, P_In_S, P_RO, st, Fp, Yopt, P_Sys, Vf, hdot, h, Theta_VFD, Vr, Alpha,Obj1, Obj2, Obj3, Obj4 = {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}
 
 for t in time:
-	P_Solar_Ref[t] = model.addVar(vtype="C", name="Solar Reference(%s)"%j)
-	P_In_G[t] = model.addVar(vtype="C", name="Grid Power(%s)"%j)
-	P_In_S[t] = model.addVar(vtype="C", name="Solar Power(%s)"%j)
-	P_RO[t] = model.addVar(vtype="C", name="RO Power(%s)"%j, lb=0, ub=2500)
-	st[t] = model.addVar(vtype="C", name="Tank Level(%s)"%j, ub = 1)
-	Fp[t] = model.addVar(vtype="C", name="Inlet Flow Rate(%s)"%j, lb=0, ub = 4)
-	Yopt[t] = model.addVar(vtype="C", name="Optimal Recovery(%s)"%j, lb=.6, ub=1)
-	P_Sys[t]= model.addVar(vtype="C", name="Pressure(%s)"%j)
-	Vf[t] = model.addVar(vtype="C", name="Clean Flow Rate(%s)"%j)
-	hdot[t]= model.addVar(vtype="C", name="Change In Tank Height(%s)"%j)
-	h[t] = model.addVar(vtype="C", name="Tank Height(%s)"%j, lb=0, ub=hmax)
-	Theta_VFD[t] = model.addVar(vtype="C", name="Frequency(%s)"%j, lb=0, ub=Theta_Max)
-	Vr[t] = model.addVar(vtype="C", name="Reject Flow Rate(%s)"%j, lb=0, ub=2)
+	P_Solar_Ref[t] = model.addVar(vtype="I", name="Solar Reference(%s)"%t)
+	P_In_G[t] = model.addVar(vtype="C", name="Grid Power(%s)"%t)
+	P_In_S[t] = model.addVar(vtype="C", name="Solar Power(%s)"%t)
+	P_RO[t] = model.addVar(vtype="C", name="RO Power(%s)"%t, lb=0, ub=2000)
+	st[t] = model.addVar(vtype="C", name="Tank Level(%s)"%t, ub = 1)
+	Fp[t] = model.addVar(vtype="C", name="Inlet Flow Rate(%s)"%t, lb=0, ub = 4)
+	Yopt[t] = model.addVar(vtype="C", name="Optimal Recovery(%s)"%t, lb=.6, ub=1)
+	P_Sys[t]= model.addVar(vtype="C", name="Pressure(%s)"%t)
+	Vf[t] = model.addVar(vtype="C", name="Clean Flow Rate(%s)"%t)
+	hdot[t]= model.addVar(vtype="C", name="Change In Tank Height(%s)"%t)
+	h[t] = model.addVar(vtype="C", name="Tank Height(%s)"%t, lb=0, ub=hmax)
+	Theta_VFD[t] = model.addVar(vtype="C", name="Frequency(%s)"%t, lb=0, ub=Theta_Max)
+	Vr[t] = model.addVar(vtype="C", name="Reject Flow Rate(%s)"%t, lb=0, ub=2)
 	
-	Alpha[t] = modle.addVar(vtype="B", name='x (%s)'%j)
+	Obj1[t] = model.addVar(vtype="C", name= "Obj1 (%s)" %t)
+	Obj2[t] = model.addVar(vtype="C", name= "Obj2 (%s)" %t)
+	Obj3[t] = model.addVar(vtype="C", name= "Obj3 (%s)" %t)
+	Obj4[t] = model.addVar(vtype="C", name= "Obj4 (%s)" %t)
+	
+	Alpha[t] = model.addVar(vtype="B", name='Binary Var (%s)'%t)
 
 # Create Constraints
 
 for t in time:
 	model.addCons(P_RO[t]==((Alpha[t]*P_In_G[t])+((1-Alpha[t])*P_In_S[t])), name="Power Balance")
 
-	model.addCons(P_Solar_Ref[t]>=0)
-
-	model.addCons(P_Solar_Ref[t]<=max(Solar_Forecast[t]))
+	model.addCons(0 <= (P_Solar_Ref[t]<= Solar_Forecast[t]), name="SolarMax (%s)" %t)
 	
-model.setObjective(quicksum(abs(P_RO[t]-P_In_G[t]) for t in time),"minimize")
+	model.addCons(P_In_G[t]==((Theta_VFD[t]/Theta_Max)*(Alpha[t]*P_Grid)), name="Grid Power Equality")
+	
+	model.addCons(P_In_S[t]==((Theta_VFD[t]/Theta_Max)*((1-Alpha[t])*P_Solar_Ref[t])), name="Solar Power Equality")
+	
+	model.addCons(P_Sys[t] == (((rho*Ap)/(Am*Km))*(Vf[t]-Vr[t]))+P_Osmotic, name="System Pressure Equality")
+	
+	model.addCons(Fp[t] == ((Vf[t]-Vr[t])*Ap), name="Flow")
+	
+	model.addCons(Yopt[t] == ((Vf[t]-Vr[t])/Vf[t]), name="Optimal Recovery")
+	
+	model.addCons(hdot[t] == ((Ap/As)*(Vf[t]-Vr[t]))-(Water_Forecast[t]/As), name="Change Tank Level")
+	
+	#model.addCons(h[t] == h[t]+hdot[t], name="Tank Level Update")
+	
+	model.addCons(st[t] == h[t]/hmax, name="Tank Percentage")
+	
+	model.addCons(Vf[t] == (Theta_VFD[t]*Vp)/Ap, name="Flow Rate Calc")
+        
+        model.addCons(P_RO[t] == (1/eta)*((P_Sys[t]*Fp[t]/Yopt[t])+.5*((Fp[t]*Fp[t]*Fp[t])/((Yopt[t]*Yopt[t]*Yopt[t])*(Ap*Ap)))*rho), name="RO Power Calc")
+        
+        model.addCons(Obj1[t] == abs((P_RO[t]-P_In_G[t])), name="Obj1")
+        
+        model.addCons(Obj2[t] == abs((P_RO[t]-P_In_S[t])), name="Obj2")
+        
+        model.addCons(Obj3[t] == abs((st[t]-s_opt)), name="Obj3")
+        
+        model.addCons(Obj4[t] == P_RO[t]/Fp[t], name="Obj4")
 
-model.optimize
+model.setObjective(quicksum((Obj1[t]+Obj2[t]+Obj3[t]+Obj4[t]) for t in time),"minimize")	
+model.optimize()
+print(model.getStatus())
+print"Objective value: ", model.getObjVal()
 
-if model.getStatus() == "optimal":
-    print("Optimal value:", model.getObjVal())
-
-    for t in time:
-        print(P_In_G[t].name, "=", model.getVal(P_In_G[t]))
-else:
-    print("Problem could not be solved to optimality")
-'''
-# Create constraints
-c = {}
-for i in Grapes:
-    c[i] = model.addCons(quicksum(Use[i,j]*x[j] for j in Blends) <= Inventory[i], name="Use(%s)"%i)
-
-# Objective
-model.setObjective(quicksum(Profit[j]*x[j] for j in Blends), "maximize")
-
-
-# Objective
-minimize cost : weight1*sum{t in DATA} (((P_RO[t]-P_In_G[t])^2))+weight2*sum{t in DATA} (((P_RO[t]-P_In_S[t])^2))+weight3*sum{t in DATA} (st[t]-sopt)^2 +weight4*sum{t in DATA} ((P_RO[t]/Fp[t]));
-
-# Constraints
-
-
-subject to Power_G {t in DATA}:
-P_In_G[t] = (Theta_VFD[t]/Theta_Max)*((Alpha)*P_Grid);
-
-subject to Power_S {t in DATA}:
-P_In_S[t] = (Theta_VFD[t]/Theta_Max)*((1-Alpha)*P_Solar_Ref[t]);
-
-subject to water1 {t in DATA}:
-P_Sys[t] = (((rho*Ap)/(Am*Km))*(Vf[t]-Vr[t]))+P_Osmotic;
-
-subject to Fp3 {t in DATA}:
-Fp[t] = (Vf[t]-Vr[t])*Ap;
-
-
-subject to Yopt3 {t in DATA}:
-Yopt[t] = (Vf[t]-Vr[t])/Vf[t];
-
-subject to xdot {t in DATA}:
-hdot[t]=((Ap/As)*(Vf[t]-Vr[t]))-(Water_Forecast[t]/As);
-
-subject to xdot1 {t in DATA: t>=2}:
-h[t] = h[t-1]+hdot[t];
-
-
-
-subject to slevel {t in DATA}:
-st[t] = h[t]/hmax;
-
-
-subject to water4 {t in DATA}:
-Vf[t] = (Theta_VFD[t]*Vp)/Ap;
-
-subject to P_RO_Val {t in DATA}:
-P_RO[t] = (1/eta)*((P_Sys[t]*Fp[t]/Yopt[t])+.5*(Fp[t]^3/(Yopt[t]^3*Ap^2))*rho);
-'''
+for t in time:
+    print "Solar Power(%s) = "  %t, model.getVal(P_In_S[t])
+    print "Grid Power(%s) = " %t, model.getVal(P_In_G[t])
+    print "RO Power(%s) = " %t, model.getVal(P_RO[t])
