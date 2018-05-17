24ac1bb4e6371c82c5d6627b2e32f94154b3841e
npwebste <30417327+npwebste@users.noreply.github.com>
2018-05-17 07:46:28 -0700

    Update day

---
+++ dev/05_17_2018/PWM_Output.py
@@ -0,0 +1,17 @@
+# Universal Power Supply Controller
+# USAID Middle East Water Security Initiative
+#
+# Developed by: Nathan Webster
+# Primary Investigator: Nathan Johnson
+#
+# Version History (mm_dd_yyyy)
+# 1.00 03_24_2018_NW
+#
+######################################################
+# Import Libraries
+from PWM_Wrapper import *
+import Parameters
+
+def PWM_Output(D_PID):
+
+    PWM_Write(Parameters.PWMPin,D_PID)
---
+++ dev/05_17_2018/VFD_Modbus_Registers.py
@@ -0,0 +1,28 @@
+reg = {
+    "WriteFunc":{
+        "Frequency_Set":269,
+		"Frequency_Max":267,
+		"Frequency_Min":268,
+		"Frequency_Acc":270,
+        "Motor_Start_Stop":8192
+        },
+    "ReadFunc":{
+        "Output_Frequency":4096,
+        "Output_Voltage":4097,
+        "Output_Current":4098,
+        "Output_Power":4120,
+        "Bus_Voltage":4100,
+        "Frequency_Set":269,
+        "Temperature":4103
+        }
+    }
+
+	
+'''
+VFD modbus value multipliers
+Freq = 100
+Current = 100
+Voltage = Actual
+Power = 10
+
+'''
